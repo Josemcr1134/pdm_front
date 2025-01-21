87facbefa517576570d5e105543bfe968734adb0
Josemcr1134 <cuelloroyopro@gmail.com>
2025-01-21 10:54:01 -0500

    departamental planning ui addeed

--- src/app/pages/dashboard/departamental-planning/departamental-planning.module.ts
+++ src/app/pages/dashboard/departamental-planning/departamental-planning.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { DepartamentalPlanningRoutingModule } from './departamental-planning-routing.module';
+import { MainComponent } from './main/main.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    MainComponent
+  ],
   imports: [
     CommonModule,
     DepartamentalPlanningRoutingModule
