9e6d7b4d460c974205c4794a7084cfd81780c1ba
Josemcr1134 <cuelloroyopro@gmail.com>
2025-01-22 13:42:31 -0500

    expenses module added

--- src/app/pages/dashboard/expenses/expenses-routing.module.ts
+++ src/app/pages/dashboard/expenses/expenses-routing.module.ts
@@ -1,7 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainComponent } from './main/main.component';
+import { InvestmentsComponent } from './investments/investments.component';
+import { FunctionalityComponent } from './functionality/functionality.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    component: MainComponent,
+    children:[
+      {
+        path:'investments',
+        component: InvestmentsComponent
+      },
+      {
+        path:'functionality',
+        component: FunctionalityComponent
+      },
+      {
+        path:'**',
+        redirectTo:'investments',
+        pathMatch:'full'
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/pages/dashboard/expenses/expenses.module.ts
+++ src/app/pages/dashboard/expenses/expenses.module.ts
@@ -2,13 +2,22 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ExpensesRoutingModule } from './expenses-routing.module';
+import { FunctionalityComponent } from './functionality/functionality.component';
+import { InvestmentsComponent } from './investments/investments.component';
+import { MainComponent } from './main/main.component';
+import { PaginationComponent } from '../../../shared/pagination/pagination.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    FunctionalityComponent,
+    InvestmentsComponent,
+    MainComponent
+  ],
   imports: [
     CommonModule,
-    ExpensesRoutingModule
+    ExpensesRoutingModule,
+    PaginationComponent
   ]
 })
 export class ExpensesModule { }
